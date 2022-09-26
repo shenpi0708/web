42a427d8ca77e527c9dff41b5428d11651854fc7
SHEN, PI-KANG <shenpi0708@gmail.com>
2022-09-26 19:45:29 +0800

    version v1.2

--- js/stop.js
+++ js/stop.js
@@ -1,10 +1,11 @@
+ROSIP = location.hostname
 ros = new ROSLIB.Ros({
     url : 'wss://'+ROSIP+':9090'
   });
   
 var msg = new ROSLIB.Topic({
 ros : ros,
-name : '/arm_msg',
+name : '/arm_msg_toarm',
 messageType : 'std_msgs/String'
 });
 
@@ -25,3 +26,16 @@ function stop(){
         msg.publish({data: "arm_stop"})  
     }
 } 
+
+var robot_state=document.getElementById("robot_state")
+
+var state = new ROSLIB.Topic({
+    ros: ros,
+    name: '/msg_toweb',
+    messageType: 'std_msgs/String'
+  });
+  state.subscribe(function (msg) {
+    console.log(msg.data)
+    robot_state.innerHTML=msg.data
+  });
+  
