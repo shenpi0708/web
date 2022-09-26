53ba78573fc241dac1a341e7e0cad7a769d321ea
SHEN, PI-KANG <shenpi0708@gmail.com>
2022-09-26 13:38:20 +0800

    final

--- js/stop.js
+++ js/stop.js
@@ -1,17 +1,27 @@
+ros = new ROSLIB.Ros({
+    url : 'wss://'+ROSIP+':9090'
+  });
+  
+var msg = new ROSLIB.Topic({
+ros : ros,
+name : '/arm_msg',
+messageType : 'std_msgs/String'
+});
+
+
+
 function stop(){
     stopbtm = document.getElementById("btnstop");
     // $('btnstop').test = '123'
     if (stopbtm.innerHTML=='STOP'){
         stopbtm.innerHTML = 'START'
-        stopbtm.style.backgroundColor='green'     
-  
+        stopbtm.style.backgroundColor='green'   
+        msg.publish({data: "arm_start"})  
     }
 
     else if (stopbtm.innerHTML=='START'){
         stopbtm.innerHTML = 'STOP'
         stopbtm.style.backgroundColor='red'
+        msg.publish({data: "arm_stop"})  
     }
-    
-    // console.log(stopbtm)
-    // console.log($('btnstop'))
-}
+} 
